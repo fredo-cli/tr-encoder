bf948a0c2d7afc5d4427f2d8cd634c950522a2f5
froggies.dk <froggies.dk@dbc95d48-0313-11de-b556-9305e745af9e>
2009-06-09 11:56:25 +0000

    add evaluation

--- formats/play-flv-1.sh
+++ formats/play-flv-1.sh
@@ -14,5 +14,17 @@ FF_AB=16
 FF_AC=1
 FF_AR=11025
 
+FF_PASS=2
+
+
+if [[ $EVALUTE == 1 ]]
+then
+
+EVALUATION=$(echo "$EVALUATION + ($FF_WIDTH * $FF_HEIGHT * $FF_FPS * $FF_PASS * ($DURATION_S - $SS))"|bc)
+
+else
+
 echo -e "\\n${BLUE}$(box "format: play-$FF_FORMAT-$PLAY_SIZE")${NC}"
 . "$APP_DIR/formats/play-$FF_FORMAT.sh" 
+
+fi
--- formats/play-mp4-5.sh
+++ formats/play-mp4-5.sh
@@ -20,6 +20,19 @@ FF_AR=44100
 FF_PRESET1="-vpre default -vpre ipod640"
 FF_PRESET2="-vpre default -vpre ipod640"
 
+FF_PASS=2
+
+
+if [[ $EVALUTE == 1 ]]
+then
+
+EVALUATION=$(echo "$EVALUATION + ($FF_WIDTH * $FF_HEIGHT * $FF_FPS * $FF_PASS * ($DURATION_S - $SS))"|bc)
+
+else
+
 echo -e "\\n${BLUE}$(box "format: play-$FF_FORMAT-$PLAY_SIZE")${NC}"
 . "$APP_DIR/formats/play-$FF_FORMAT.sh" 
+
+fi
+
 fi
