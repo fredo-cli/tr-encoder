bea44565eaeda1fb30b8fc302379dad1c6a9e942
froggies.dk <froggies.dk@dbc95d48-0313-11de-b556-9305e745af9e>
2009-07-29 14:11:24 +0000

    add 3gp

--- lib/install/freebsd_install.sh
+++ lib/install/freebsd_install.sh
@@ -1,412 +1,218 @@
 #!/usr/local/bin/bash
 
-LAME_VERSION=3.98
-FFMPEG_VERSION=17737
-X264_VERSION=0.65
-MPLAYER_VERSION=29242
 
-
-echo "add fadd imagemagick"
-exit
 
 function PORTSNAP(){
 sudo su -
 # install portsnap if needed
-#pkg_add -r portsnap
-#mkdir /usr/ports
-#portsnap fetch
-#portsnap extract
-
-portsnap fetch
-portsnap update
-}
-
-
-### lame
-
-function INSTALL_LAME(){
-#http://fastbull.dl.sourceforge.net/sourceforge/lame/lame-398.tar.gz
-cd
-wget http://freefr.dl.sourceforge.net/sourceforge/lame/lame-397.tar.gz -O - | tar xzf -
-cd lame-398
-./configure --enable-shared --enable-static
-sudo gmake
-sudo gmake install
 } 
 
 
-function INSTALL_MPLAYER(){
+
+### mplayer
+
+function INSTALL_MPLAYER_SVN(){
 
 cd  
 svn checkout -r  $MPLAYER_VERSION  svn://svn.mplayerhq.hu/mplayer/trunk mplayer
 cd mplayer
-./configure
-gmake
 sudo  su root -c 'gmake install'
 }
 
-function INSTALL_FFMPEG(){
 
 
-cd  $HOME
-[[ -d ffmpeg ]] && echo -e "${yellow}# clean ffmpeg*${NC}" &&  sudo rm -rf ffmpeg*
 
-wget http://dl.getdropbox.com/u/221284/ffmpeg.tar.gz
-
-tar -xzvf ffmpeg.tar.gz 
-
-cd  /usr/ports/multimedia/ffmpeg/work/ffmpeg-2008-07-27
-
-FFMPEG_VERSION=17727
-svn checkout -r $FFMPEG_VERSION svn://svn.ffmpeg.org/ffmpeg/trunk .
-
-
-#patch wma3 a
-
-echo -e "${yellow}# add patch wma3${NC}" 
-
-cd libavcodec
-ln -s ../../wma3dec.c wma3dec.c
-ln -s ../../wma3data.h wma3data.h
-ln -s ../../wma3.h wma3.h
-cd ../
-patch -p0 <../wmapro_ffmpeg.patch
-patch -p0 <../audioframesize.patch
-
-# patch pip
-
-echo -e "${yellow}# add patch pip${NC}" 
-cd ./vhook
-ln -s  ../../pip1.2.1.c pip.c
-cd ../
-patch -p0  <  ../pip.patch
-
- 
-
-exit
-echo "${yellow}# add patch freebsd${NC}" 
-cd .. 
-wget http://www.nabble.com/attachment/22286995/0/ffmpeg.bsd.patch
-patch -p0 < ffmpeg.bsd.patch 
-cd  /usr/ports/multimedia/ffmpeg/work/ffmpeg-2008-07-27
-
-
-echo -e "${yellow}# configure${NC}" 
-export LIBRARY_PATH=/usr/local/lib
-export CPATH=/usr/local/include
-
-#./configure --cc=cc --prefix=/usr/local --disable-debug --enable-memalign-hack --enable-shared --enable-postproc --extra-cflags="-I/usr/local/include/vorbis -I/usr/local/include" --extra-ldflags="-L/usr/local/lib -la52" --extra-libs=-pthread --enable-gpl --enable-pthreads  --mandir=/usr/local/man  --enable-libfaac --enable-libfaad --enable-libfaadbin --enable-libamr-nb --enable-nonfree --disable-libamr-wb --enable-nonfree --disable-mmx --disable-libgsm --enable-libmp3lame --disable-ffplay --enable-libtheora --enable-libvorbis --enable-libx264 --enable-libxvid --disable-ipv6
-# --enable-swscale
-#./configure --cc=cc --prefix=/usr/local --disable-debug --enable-memalign-hack --enable-shared --enable-postproc --extra-cflags="-I/usr/local/include/vorbis -I/usr/local/include" --extra-ldflags="-L/usr/local/lib -la52" --extra-libs=-pthread --enable-gpl --enable-pthreads  --mandir=/usr/local/man  --enable-libfaac --enable-libfaad --enable-libfaadbin --enable-libamr-nb --enable-nonfree --disable-libamr-wb --enable-nonfree --disable-mmx --disable-libgsm --enable-libmp3lame --disable-ffplay --enable-libtheora --enable-libvorbis --enable-libx264 --enable-libxvid --disable-ipv6 --enable-swscale
-
-# work!
-./configure --prefix=$HOME/ffmpeg-pip --enable-libfaac --enable-libfaad  --enable-libfaadbin --enable-libmp3lame --enable-libgsm  --enable-libamr_nb --enable-libamr_wb  --enable-libvorbis --enable-libtheora  --enable-libx264 --enable-libxvid  --enable-nonfree  --enable-swscale    --disable-shared  --disable-debug  --enable-static --disable-devices --enable-gpl --enable-postproc --enable-pthreads   --enable-memalign-hack --disable-mmx   --disable-ffplay  --disable-ffserver --disable-ipv6 --extra-cflags="-I/usr/local/include/vorbis -I/usr/local/include" --extra-ldflags="-L/usr/local/lib -la52" --extra-libs=-pthread
-
-
-# compile using GNU Make (gmake), not BSD Make
-echo -e "${yellow}# gmake${NC}" 
-gmake
-exit
-# install
-
-echo -e "${yellow}# gmake install${NC}" 
-sudo  su root -c 'gmake install'
-
-
-}
 
 function INSTALL_FFMPEG_old(){
 
 
-
-cd 
-[[ -d ffmpeg ]] && echo -e "${yellow}# clean ffmpeg*${NC}" &&  rm -rf ffmpeg*
-
-wget http://dl.getdropbox.com/u/221284/ffmpeg.tar.gz
-tar -xzvf ffmpeg.tar.gz 
-
-cd ffmpeg
-
-echo -e "${yellow}# checkout version $FFMPEG_VERSIONff from ffmpeg${NC}"
-
-# 17727 0.5
-# 17768 last version with vhook 
-# 14424 freebsd
-FFMPEG_VERSION=17727
-svn checkout -r $FFMPEG_VERSION svn://svn.ffmpeg.org/ffmpeg/trunk ffmpeg
- 
-
-
-#patch wma3 
-
-echo -e "${yellow}# add patch wma3${NC}" 
-
-cd ffmpeg/libavcodec
-ln -s ../../wma3dec.c wma3dec.c
-ln -s ../../wma3data.h wma3data.h
-ln -s ../../wma3.h wma3.h
-cd ../
-patch -p0 <../wmapro_ffmpeg.patch
-patch -p0 <../audioframesize.patch
-
-# patch pip
-
-echo -e "${yellow}# add patch pip${NC}" 
-cd ./vhook
-ln -s  ../../pip1.2.1.c pip.c
-cd ../
-patch -p0  <  ../pip.patch
-
- 
-
-
-echo "${yellow}# add patch freebsd${NC}" 
-cd .. 
-wget http://www.nabble.com/attachment/22286995/0/ffmpeg.bsd.patch
-patch -p0 < ffmpeg.bsd.patch 
-cd ffmpeg
-
-
-echo -e "${yellow}# configure${NC}" 
-export LIBRARY_PATH=/usr/local/lib
-export CPATH=/usr/local/include
-
-#./configure --cc=cc --prefix=/usr/local --disable-debug --enable-memalign-hack --enable-shared --enable-postproc --extra-cflags="-I/usr/local/include/vorbis -I/usr/local/include" --extra-ldflags="-L/usr/local/lib -la52" --extra-libs=-pthread --enable-gpl --enable-pthreads  --mandir=/usr/local/man  --enable-libfaac --enable-libfaad --enable-libfaadbin --enable-libamr-nb --enable-nonfree --disable-libamr-wb --enable-nonfree --disable-mmx --disable-libgsm --enable-libmp3lame --disable-ffplay --enable-libtheora --enable-libvorbis --enable-libx264 --enable-libxvid --disable-ipv6
-# --enable-swscale
-./configure --cc=cc --prefix=/usr/local --disable-debug --enable-memalign-hack --enable-shared --enable-postproc --extra-cflags="-I/usr/local/include/vorbis -I/usr/local/include" --extra-ldflags="-L/usr/local/lib -la52" --extra-libs=-pthread --enable-gpl --enable-pthreads  --mandir=/usr/local/man  --enable-libfaac --enable-libfaad --enable-libfaadbin --enable-libamr-nb --enable-nonfree --disable-libamr-wb --enable-nonfree --disable-mmx --disable-libgsm --enable-libmp3lame --disable-ffplay --enable-libtheora --enable-libvorbis --enable-libx264 --enable-libxvid --disable-ipv6 --enable-swscale
-
-
-# compile using GNU Make (gmake), not BSD Make
-echo -e "${yellow}# gmake${NC}" 
-gmake
-
-# install
-
-echo -e "${yellow}# gmake install${NC}" 
-sudo  su root -c 'gmake install'
-
-
-}
-
-function INSTALL_TR-ENCODER(){
-cd 
-svn checkout http://tr-encoder.googlecode.com/svn/trunk/ tr-encoder
-sudo ln -sf /home/fred/tr-encoder/tr-encoder.sh /usr/bin/tr-encoder
 chmod +x /home/fred/tr-encoder/tr-encoder.sh
 }
+
+
+
+
 
 # Controle the version of mplayer
 
 echo -en "mplayer\t"
-MPLAYER_VERSION_DETECTED=$(mplayer |grep -o "MPlayer SVN-r$MPLAYER_VERSION" |grep -o "$MPLAYER_VERSION")
+MPLAYER_VERSION_DETECTED=$(mplayer|head -1 |awk '{print $2 }'
+)
 
 
 if [[ "$MPLAYER_VERSION_DETECTED" != "$MPLAYER_VERSION" ]]
-then 
-echo -e "${yellow}false${NC}\t($MPLAYER_VERSION_DETECTED)"
-[[ $INSTALL == 1 ]] && INSTALL_MPLAYER
-else
-echo -e "${green}true${NC}\t($MPLAYER_VERSION_DETECTED)"
-[[ $REINSTALL == 1 ]] && INSTALL_MPLAYER
 fi
 
 
 
 
-
-
-
-# Controle the version of lame
-
-echo -en "lame\t"
-LAME_VERSION_DETECTED=$(lame --longhelp|grep -o "version [0-9.]\{4,\}")
-
-#if [[ -z $(lame --longhelp|grep "version" |grep  -o "3.98 ") ]]
-if [[ -z $(pkg_version  -vs ^lame- |grep  -o $LAME_VERSION) ]]
-then 
-echo -e "${yellow}false${NC}\t($LAME_VERSION_DETECTED)"
-[[ $INSTALL == 1 ]] && INSTALL_LAME
-else
-echo -e "${green}true${NC}\t($LAME_VERSION_DETECTED)"
-[[ $REINSTALL == 1 ]] && INSTALL_LAME
-fi
-
-
-# Controle if faac  is install
-
-echo -en "faac\t"
-if [[ ! -z $(which faac) ]]
-then
-echo -e "${green}true${NC}\t($(pkg_version  -vs ^faac- |awk -F " " '{print $1}'))"
-[[ $REINSTALL == 1 ]] && cd /usr/ports/audio/faac/ && make deinstall && sudo make install clean
-else
-echo -e "${yellow}false${NC}\t"
-[[ $INSTALL == 1 ]] && cd /usr/ports/audio/faac/ && sudo make install clean
-fi
-
-
-# Controle if faacd  is install
-
-echo -en "faad\t"
-if [[ ! -z $(which faad) ]]
-then
-echo -e "${green}true${NC}\t($(pkg_version  -vs ^faad2- |awk -F " " '{print $1}'))"
-[[ $REINSTALL == 1 ]] && cd /usr/ports/audio/faad/ && make deinstall && sudo make install clean
-else
-echo -e "${yellow}false${NC}\t"
-[[ $INSTALL == 1 ]] && cd /usr/ports/audio/faad/ && sudo make install clean
-fi
-
-
-
-
-# Controle if AtomicParsley  is install
-
-echo -en "AtomicParsley\t"
-if [[ ! -z $(which AtomicParsley) ]]
-then
-echo -e "${green}true${NC}\t($(pkg_version  -vs ^AtomicParsley |awk -F " " '{print $1}'))"
-[[ $REINSTALL == 1 ]] && cd /usr/ports/multimedia/atomicparsley/ && make deinstall && sudo make install clean
-else
-echo -e "${yellow}false${NC}\t($(pkg_version  -vs ^AtomicParsley |awk -F " " '{print $1}'))"
-[[ $INSTALL == 1 ]] && cd /usr/ports/multimedia/atomicparsley/ && sudo make install clean
-fi
-
-
-# Controle if ruby-flvtool2  is install
+### ruby-flvtool2
 
 echo -en "ruby-flvtool2\t"
 if [[ ! -z $(which flvtool2) ]]
 then
-echo -e "${green}true${NC}\t($(pkg_version  -vs ^ruby-flvtool2 |awk -F " " '{print $1}'))"
-[[ $REINSTALL == 1 ]] && cd /usr/ports/multimedia/ruby-flvtool2/ && make deinstall && sudo make install clean
-else
-echo -e "${yellow}false${NC}\t($(pkg_version  -vs ^ruby-flvtool2 |awk -F " " '{print $1}'))"
-[[ $INSTALL == 1 ]] && cd /usr/ports/multimedia/ruby-flvtool2/ && sudo make install clean
 fi
 
 
-# Controle if ruby-flvtool2  is install
+
+
+
+
+### sox
 
 echo -en "sox\t"
-if [[ ! -z $(which flvtool2) ]]
+
+if [[ ! -z $(which sox) ]]
 then
+
 echo -e "${green}true${NC}\t($(pkg_version  -vs ^sox- |awk -F " " '{print $1}'))"
 [[ $REINSTALL == 1 ]] && cd /usr/ports/audio/sox && make deinstall && sudo make install clean
+
 else
-echo -e "${yellow}false${NC}\t($(pkg_version  -vs ^sox- |awk -F " " '{print $1}'))"
+
+echo -e "${yellow}false${NC}\t"
 [[ $INSTALL == 1 ]] && cd /usr/ports/audio/sox && sudo make install clean
+
 fi
 
 
 
-# Controle if mediainfo  is install
+
+### mediainfo
 
 echo -en "mediainfo\t"
+
 if [[ ! -z $(which mediainfo) ]]
 then
+
 echo -e "${green}true${NC}\t($(pkg_version  -vs ^mediainfo- |awk -F " " '{print $1}'))"
 [[ $REINSTALL == 1 ]] && cd /usr/ports/multimedia/mediainfo/ && make deinstall && sudo make install clean
+
 else
+
 echo -e "${yellow}false${NC}\t($(pkg_version  -vs ^mediainfo- |awk -F " " '{print $1}'))"
 [[ $INSTALL == 1 ]] && cd /usr/ports/multimedia/mediainfo/ && sudo make install clean
+
 fi
+
+
+
+
+### mpabox
 
 echo -en "gpac-mp4box\t"
+
 if [[ ! -z $(which mp4box) ]]
 then
+
 echo -e "${green}true${NC}\t($(pkg_version  -vs ^gpac-mp4box- |awk -F " " '{print $1}'))"
 [[ $REINSTALL == 1 ]] && cd /usr/ports/multimedia/gpac-mp4box && make deinstall && sudo make install clean
+
 else
+
 echo -e "${yellow}false${NC}\t($(pkg_version  -vs ^gpac-mp4box- |awk -F " " '{print $1}'))"
 [[ $INSTALL == 1 ]] && cd /usr/ports/multimedia/gpac-mp4box && sudo make install clean
+
 fi
 
 
 
 
-
-# Controle if readlink  is install
+### readlink
 
 echo -en "readlink\t"
+
 if [[ ! -z $(which mediainfo) ]]
 then
+
 echo -e "${green}true${NC}\t($(pkg_version  -vs ^readlink- |awk -F " " '{print $1}'))"
 [[ $REINSTALL == 1 ]] && cd /usr/ports/sysutils/readlink && make deinstall && sudo make install clean
+
 else
+
 echo -e "${yellow}false${NC}\t($(pkg_version  -vs ^readlink- |awk -F " " '{print $1}'))"
 [[ $INSTALL == 1 ]] && cd /usr/ports/sysutils/readlink && sudo make install clean
+
 fi
 
-# Controle if seq2  is install
+
+### seq2
 
 echo -en "seq2\t"
+
 if [[ ! -z $(which seq2) ]]
 then
+
 echo -e "${green}true${NC}\t($(pkg_version  -vs ^seq2- |awk -F " " '{print $1}'))"
 [[ $REINSTALL == 1 ]] && cd /usr/ports/misc/seq2 && make deinstall && sudo make install clean
+
 else
+
 echo -e "${yellow}false${NC}\t($(pkg_version  -vs ^seq2- |awk -F " " '{print $1}'))"
 [[ $INSTALL == 1 ]] && cd /usr/ports/misc/seq2 && sudo make install clean
+
 fi
 
 
-# Controle if md5  is install
+
+
+
+### md5
 
 echo -en "md5 (cfv)\t"
-if [[  ! -z $(pkg_version  -vs ^cfv- |awk -F " " '{print $1}' | grep "pkg_version: no packages match pattern") ]]
+
+if [[ -z $(which cfv) ]]
 then
+
+
 echo -e "${yellow}false${NC}\t"
 [[ $INSTALL == 1 ]] && cd /usr/ports/security/cfv && sudo make install clean
+
 else
+
 echo -e "${green}true${NC}\t($(pkg_version  -vs ^cfv- |awk -F " " '{print $1}'))"
 [[ $REINSTALL == 1 ]] && cd /usr/ports/security/cfv && make deinstall && sudo make install clean
+
 fi
 
 
 
-# Controle if x264  is install and the check version
+### x264
 
 echo -en "x264\t"
 if [[ ! -z $(which x264) && $(x264 --version |grep x264 |grep -o [0-9]\\.[0-9]*) == $X264_VERSION ]]
 then
+
 echo -e "${green}true${NC}\t($(pkg_version  -vs ^x264- |awk -F " " '{print $1}')) $(x264 --version |grep x264 |grep -o [0-9]\\.[0-9]*)"
 [[ $REINSTALL == 1 ]] && cd /usr/ports/multimedia/x264 && make deinstall && sudo make install clean
+
 else
+
 echo -e "${yellow}false${NC}\t"
+
 [[ $INSTALL == 1 ]] && cd /usr/ports/multimedia/x264 && sudo make install clean
-fi
-
-
 # old
 # wget -O x264.patch  http://www.freebsd.org/cgi/query-pr.cgi?prp=132780-1-txt&n=/x264.patch 
 # patch -p0 < x264.patch
 # make install clean
 
-
-echo -en "ffmpeg\t"
-FFMPEG_INSTALLED=$(ffmpeg -i 2>&1 |grep FFmpeg |grep -o SVN-r[0-9]* )
-if [[ $FFMPEG_INSTALLED != "SVN-r$FFMPEG_VERSION" ]]
-then
-echo -e "${yellow}false${NC}\t($FFMPEG_INSTALLED)"
-[[ $INSTALL == 1 ]] && INSTALL_FFMPEG
-else
-echo -e "${green}true${NC}\t($FFMPEG_INSTALLED)"
 fi
 
 
-### end
 
-cd
+### ffmpeg
 
-echo "Press any keys to exit"
-[[ $INSTALL == 0 ]] && echo "bash $0 -i to install missing packages"
-read
-exit
+echo -en "ffmpeg\t"
 
+FFMPEG_INSTALLED=$(ffmpeg -i 2>&1 |grep FFmpeg |grep -o SVN-r[0-9]* )
+if [[ $FFMPEG_INSTALLED != "SVN-r$FFMPEG_VERSION" ]]
+then
 
+echo -e "${yellow}false${NC}\t($FFMPEG_INSTALLED)"
+[[ $INSTALL == 1 ]] && INSTALL_FFMPEG
 
+else
 
+echo -e "${green}true${NC}\t($FFMPEG_INSTALLED)"
 
-
-
+fi
