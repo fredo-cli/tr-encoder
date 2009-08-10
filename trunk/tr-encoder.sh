bebced3635ac1d1d3bdc396acd1858622e48dcd6
froggies.dk <froggies.dk@dbc95d48-0313-11de-b556-9305e745af9e>
2009-08-10 13:16:24 +0000

    change ntsc 1.33

--- trunk/tr-encoder.sh
+++ trunk/tr-encoder.sh
@@ -801,18 +801,21 @@ if [[ ( $RATIO_I  -ge 122 && $RATIO_I -le 128 ) && ( $DAR == 0 || $DAR  == 1.25 
 		 
 	      if [[ $RATIO_I == 150  &&  $DAR == 1.33 ]]
 	      then 
-		 DETECTED_FORMAT="1.50 - ntsc DAR 1.33"
-		 echo -e "${green}# Format: $DETECTED_FORMAT${NC}"
 
-	      # Cropping: no
+		  DETECTED_FORMAT="1.50 - ntsc DAR 1.33"
+		  echo -e "${green}# Format: $DETECTED_FORMAT${NC}"
+
+
+		  CROPLEFT=0              
+		  CROPRIGHT=0
+		  FF_CROP_WIDTH=""
+		  CROPTOP=66
+		  CROPBOTTOM=66
+		  FF_CROP_HEIGHT="-croptop $CROPTOP -cropbottom $CROPBOTTOM "
+
+		  # Cropping: yes
 		 
-	      [[ $DEBUG -gt 0 ]] && echo -e "${cyan}# Cropping: no${NC}"		 
-	      CROPLEFT=0              
-	      CROPRIGHT=0
-	      FF_CROP_WIDTH=""
-	      CROPTOP=66
-	      CROPBOTTOM=66
-	      FF_CROP_HEIGHT="-croptop $CROPTOP -cropbottom $CROPBOTTOM "
+		  [[ $DEBUG -gt 0 ]] && echo -e "${cyan}# Cropping: FF_CROP_HEIGHT${NC}"
 		 
 		  # distortion 0.888
 		  DISTORTION="`echo "scale=3; $ID_VIDEO_ASPECT  / $RATIO "|bc` "
