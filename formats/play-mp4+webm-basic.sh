5bd30bd1372e56cff68b4868a8693645ac5ab011
frederic.terrible@gmail.com <frederic.terrible@gmail.com@dbc95d48-0313-11de-b556-9305e745af9e>
2014-06-24 15:04:37 +0000

    space issue

--- formats/play-mp4+webm-basic.sh
+++ formats/play-mp4+webm-basic.sh
@@ -1,8 +1,8 @@
 #!/usr/local/bin/bash
 . "$APP_DIR/formats/play-mp4-3.sh" 
-. "$APP_DIR/formats/play-mp4-4.sh"
-. "$APP_DIR/formats/play-mp4-5.sh"
+. "$APP_DIR/formats/play-mp4-4.sh" 
+. "$APP_DIR/formats/play-mp4-5.sh" 
 
-. "$APP_DIR/formats/play-webm-3.sh"
-. "$APP_DIR/formats/play-webm-4.sh"
-. "$APP_DIR/formats/play-webm-5.sh"
+. "$APP_DIR/formats/play-webm-3.sh" 
+. "$APP_DIR/formats/play-webm-4.sh" 
+. "$APP_DIR/formats/play-webm-5.sh" 
